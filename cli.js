b6ca81a107dd26a1e2f7cf4a9fcee3481c47a80f
interisti <nika.interisti@gmail.com>
2018-06-13 21:17:48 +0400

    exit process with exitCode from the command

--- cli.js
+++ cli.js
@@ -2,4 +2,5 @@
 
 const shell = require('shelljs');
 require('./index')();
-shell.exec(process.argv.slice(2).join(' '));
+const result = shell.exec(process.argv.slice(2).join(' '));
+process.exit(result.code)
